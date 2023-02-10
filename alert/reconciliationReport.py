7b145f1975c6ed2cf488ad617064dc1a3d50a8c0
apickens2 <ahudson2@umd.edu>
2023-02-10 15:18:17 -0500

    removed extra spaces in paths

--- alert/reconciliationReport.py
+++ alert/reconciliationReport.py
@@ -6,18 +6,18 @@ import sys
 reportDate = today = (datetime.datetime.utcnow() + datetime.timedelta(days=-2)).strftime("%Y%m%d")
 
 def send():
-  reconciliationFile = "/gpfs/glad3/HLSDIST/LP-DAAC/ingestReports/reconciliation. json"
+  reconciliationFile = "/gpfs/glad3/HLSDIST/LP-DAAC/ingestReports/reconciliation.json"
 
-  if os.path.exists("/gpfs/glad3/HLSDIST/LP-DAAC/ingestReports/sentToLP_" +reportDate+".rpt"):
+  if os.path.exists("/gpfs/glad3/HLSDIST/LP-DAAC/ingestReports/sentToLP_"+reportDate+".rpt"):
     with open(reconciliationFile,'w') as noti:
-      noti.write("{\"report\": {\"uri\": \"https://glad.umd.edu/projects/opera/ ingestReports/sentToLP_" +reportDate+".rpt\"}}\n")
+      noti.write("{\"report\": {\"uri\": \"https://glad.umd.edu/projects/opera/ingestReports/sentToLP_"+reportDate+".rpt\"}}\n")
 
-    response = subprocess.run(["module load awscli;source /gpfs/glad3/HLSDIST/  System/user.profile; aws sns  publish --region 'us-west-2' --topic-arn    arn:aws:sns:us-west-2:643705676985:lp-prod-reconciliation-notification   --message file://" +reconciliationFile],capture_output=True,shell=True)
+    response = subprocess.run(["module load awscli;source /gpfs/glad3/HLSDIST/System/user.profile; aws sns publish --region 'us-west-2' --topic-arn arn:aws:sns:us-west-2:643705676985:lp-prod-reconciliation-notification --message file://" +reconciliationFile],capture_output=True,shell=True)
 
     if response.stderr.decode().strip() != "":
       with open("errorLOG.txt",'a') as ERR:
         now = datetime.datetime.now()
-        ERR.write("Error in sendReconcilicationReport.py "+str(now)+response.stderr.decode())
+        ERR.write("Error in reconcilicationReport.py "+str(now)+response.stderr.decode())
 
 def receive():
   if os.path.exists("/gpfs/glad3/HLSDIST/LP-DAAC/ingestReports/sentToLP_" +reportDate+".rpt"):
